5c98deb1256d8a9343707965e7b81c08177fe883
reaganongaya <reagan.ongaya@student.moringaschool.com>
2023-02-11 23:16:15 +0300

    Assignment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,43 @@ require_relative '../lib/person'
 RSpec.configure do |config|
   config.order = :default
 end
+
+class Dog
+  def bark
+    puts "Woof!"
+
+  end 
+end
+
+fido = Dog.new
+fido.bark
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+
+  end 
+end
+
+fido = Dog.new
+fido.sit
+
+class Person 
+  def talk
+    puts "Hello World!"
+
+  end
+end 
+
+ada = Person.new
+ada.talk
+
+class Person 
+  def walk
+    puts "The Person is walking"
+
+  end
+end 
+
+ada = Person.new
+ada.walk
